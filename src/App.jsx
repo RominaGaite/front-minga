19996682c16f842894bca57e9469788abe486377
Romina Gaite <gaiteromina@gmail.com>
2023-02-27 12:32:30 -0300

    formulario registro

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
+import "./App.css";
+import FormRegister from "./components/FormRegister/FormRegister";
 
-
-import DivTrigun from './components/DivTrigun/DivTrigun';
-import NavBar from './components/NavBar/NavBar';
-import DivBoton from './components/DivBoton/DivBoton';
-import "./App.css"
 
 
 function App() {
   return (
-      <div className="app-container">
-        <NavBar/>
-        <DivTrigun/>
-        <DivBoton/>
+  
+      <div className="divForm">
+        <FormRegister />
+        <div className="img-form"></div>
       </div>
+    
   );
 }
 export default App;
